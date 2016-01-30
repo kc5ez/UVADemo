d806d808b47ae69847337417b9a280ab489858dc
Ke Cheng <Ke.Cheng@capitalone.com>
2016-01-29 22:15:38 -0800

    5 uva things to do

---
+++ implementations/Nicklomentation/CageApi.js
@@ -0,0 +1,31 @@
+class CageApi {
+    constructor() {
+        this.cages = ["http://assets.uvamagazine.org/images/uploads/2013/DeanGrovesWorldRecord/MoreHighFives.jpg", 
+            "https://aiasatuva.files.wordpress.com/2014/01/humpback2.jpg",
+            "https://scontent-lax3-1.xx.fbcdn.net/hphotos-ash2/v/t1.0-9/10502049_10152891032437068_8151873515752733152_n.jpg?oh=340d7c5637de2b21d260eb63e2aafbee&oe=572E7393", 
+            "https://i.ytimg.com/vi/woCzT0Pxy8Y/hqdefault.jpg", 
+            "http://www.hercampus.com/sites/default/files/2013/04/25/Foxfield.jpg"];
+        this.colors = ['orange', 'orange', 'orange', 'orange', 'orange']
+        this.words = ["High-five Dean Groves", "Watch the Sunrise \n from Humpback Rock", "Take a picture \n with Cav Man", "See the \n Lighting of the Lawn", "Appreciate a Horse at \n Foxfield"];
+        this.i = 0;
+    }
+
+    goToNext() {
+        let index = this.i++;
+        return {
+            cage: this.cages[index % this.cages.length],
+            color: this.colors[index % this.colors.length],
+            quote: this.words[index % this.words.length]
+        }
+    }
+
+    onReject(data) {
+        console.log('rejected' + JSON.stringify(data));
+    }
+
+    onAccept(data) {
+        console.log('accepted' + JSON.stringify(data));
+    }
+}
+
+export default CageApi;
---
+++ index.ios.js
@@ -0,0 +1,30 @@
+import React, {
+	AppRegistry, StyleSheet, Text, View, Animated, Component, PanResponder,
+}
+from 'react-native';
+import CardSwiperContainer from './containers/CardSwiperContainer';
+import SimpleApi from './implementations/BasicSimplifiedImplementation/SimpleApi'
+import SimpleView from './implementations/BasicSimplifiedImplementation/SimpleView'
+import CageView from './implementations/Nicklomentation/CageView'
+import CageApi from './implementations/Nicklomentation/CageApi'
+
+class mainView extends Component {
+	constructor(props) {
+		super(props);
+		this.cardApi = new CageApi()
+	}
+
+	render() {
+		return (
+			<View style={{flex:1}}>
+				<CardSwiperContainer  acceptAnimation={true} rejectAnimation={true} acceptText={"Done!"} rejectText={"Not yet!"}
+					onReject={this.cardApi.onReject.bind(this.cardApi)} onAccept={this.cardApi.onAccept.bind(this.cardApi)} 
+					getNextCardDataSource={this.cardApi.goToNext.bind(this.cardApi)}>
+					<CageView/>
+				</CardSwiperContainer>
+			</View>
+		);
+	}
+}
+
+AppRegistry.registerComponent('ReactNativeCardSwiper', () => mainView);
